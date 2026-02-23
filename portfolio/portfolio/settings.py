f28254936fc5fb6af2dd765857f1f18c6a434c88
Omkar2629 <omkarsubhankar263@gmail.com>
2026-02-23 20:47:48 +0530

    Security updates

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -87,14 +87,21 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 #     }
 # }
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'HOST': DB_HOST,
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
